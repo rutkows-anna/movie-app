e740e27d217d1595fa1300a2b1da23c8d14272bc
Anna Rutkowska <rutkows.anna@gmail.com>
2020-12-01 14:16:23 +0100

    Updates

---
+++ precache-manifest.a94a155a7f196bf3099f001804e7bc62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775fbe1fa5e3142d08b73c06d8f19e63",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "4b52a112507c68a56e54",
+    "url": "/movie-app/static/css/main.2492c244.chunk.css"
+  },
+  {
+    "revision": "669e1274aab06a3c7f9c",
+    "url": "/movie-app/static/js/2.7e2925d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/static/js/2.7e2925d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b52a112507c68a56e54",
+    "url": "/movie-app/static/js/main.d85e5919.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  },
+  {
+    "revision": "68a85f42450e4ceb489d288366c696e5",
+    "url": "/movie-app/static/media/videographer.68a85f42.svg"
+  }
+]);
