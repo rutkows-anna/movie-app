dc33f10cfcbf9535d3bed786206798dd9fe22f9d
Anna Rutkowska <rutkows.anna@gmail.com>
2020-12-01 13:29:49 +0100

    delete night icons

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { routes } from "../../routes/routes";
 import styles from "./Nav.module.css";
-import night from "../../assets/night.svg";
 
 function Nav() {
   return (
@@ -37,18 +36,6 @@ function Nav() {
           >
             Watchlist
           </NavLink>
-        </li>
-        <li>
-          <button
-            className={styles.nightMode}
-            onClick={() => console.log("night mode")}
-          >
-            <img
-              src={night}
-              alt="night mode"
-              className={styles.nightModeIcon}
-            />
-          </button>
         </li>
       </ul>
     </nav>
